ca9f7b83d8ad9a0834cf6a66cab8c96465596d07
visha12l <vsvshinde15@gmail.com>
2018-01-11 14:07:54 +0530

    adds html assignment

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var button = document.getElementById('hamburgerButton');
+var video = document.getElementById("videoWrap"); 
+var playPauseButton = document.getElementById('playPauseButton');
+
+button.addEventListener('click',  toggleClass = () => {
+    button.classList.toggle('active');
+});
+
+playPauseButton.addEventListener('click',  playPause = () => { 
+    if (video.paused) {
+        video.play();
+        playPauseButton.classList.add('active'); 
+    } else {
+        video.pause();
+        playPauseButton.classList.remove('active'); 
+    }
+});
